cbda7ec653e8de00ee986a35b4f2134b7b96fde0
Shri Harrsha Samala <shriharrsha1@gmail.com>
2021-09-29 21:49:53 +0530

    commit-1

---
+++ sound pollution activity/sound.js
@@ -0,0 +1,87 @@
+function BO() 
+{
+  var progressbar = document.getElementById("pollution");
+  var total = 65;
+  console.log(total);
+  progressbar.style.backgroundColor = "green";
+  progressbar.style.width = (total-13) * 1 + "%";
+  progressbar.innerText = parseInt((total) * 1) + "db";
+}
+function BR() 
+{
+    var progressbar = document.getElementById("pollution");
+    var total = 25;
+    progressbar.style.backgroundColor = "blue";
+    progressbar.style.width = (total-10) * 1 + "%";
+    progressbar.innerText = parseInt((total) * 1) + "db";
+}
+function HT() 
+{
+    var progressbar = document.getElementById("pollution");
+    var total = 90;
+    progressbar.style.backgroundColor = "red";
+    progressbar.style.width = (total-20) * 1 + "%";
+    progressbar.innerText = parseInt((total) * 1) + "db";
+}
+function CS() 
+{
+      var progressbar = document.getElementById("pollution");
+      var total = 60;
+      console.log(total);
+      progressbar.style.backgroundColor = "green";
+      progressbar.style.width = (total-20) * 1 + "%";
+      progressbar.innerText = parseInt((total) * 1) + "db";
+}
+function JE() 
+{
+        var progressbar = document.getElementById("pollution");
+        var total = 140;
+        console.log(total);
+        progressbar.style.backgroundColor = "darkred";
+        progressbar.style.width = (total-44) * 1 + "%";
+        progressbar.innerText = parseInt((total) * 1) + "db";
+}
+function JH() 
+{
+        var progressbar = document.getElementById("pollution");
+        var total = 110;
+        console.log(total);
+        progressbar.style.backgroundColor = "darkred";
+        progressbar.style.width = (total-25) * 1 + "%";
+        progressbar.innerText = parseInt((total) * 1) + "db";
+}
+function LR() 
+{
+        var progressbar = document.getElementById("pollution");
+        var total = 40;
+        console.log(total);
+        progressbar.style.backgroundColor = "green";
+        progressbar.style.width = (total-15) * 1 + "%";
+        progressbar.innerText = parseInt((total) * 1) + "db";
+}
+function LRM() 
+{
+        var progressbar = document.getElementById("pollution");
+        var total = 100;
+        console.log(total);
+        progressbar.style.backgroundColor = "red";
+        progressbar.style.width = (total-25) * 1 + "%";
+        progressbar.innerText = parseInt((total) * 1) + "db";
+}
+function ST() 
+{
+        var progressbar = document.getElementById("pollution");
+        var total = 85;
+        console.log(total);
+        progressbar.style.backgroundColor = "red";
+        progressbar.style.width = (total-20) * 1 + "%";
+        progressbar.innerText = parseInt((total) * 1) + "db"
+}
+function WL() 
+{
+        var progressbar = document.getElementById("pollution");
+        var total = 10;
+        progressbar.style.backgroundColor = "blue";
+        progressbar.style.width = (total-3) * 1 + "%";
+        progressbar.innerText = parseInt((total) * 1) + "db"
+}
